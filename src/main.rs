1473efb0286fd8f4ff26a7a314c4d0fe52dc8a5d
Conan Bitter <conan.bitter@gmail.com>
2025-11-18 19:15:05 +0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use sdl3::event::Event;
+use sdl3::image::LoadSurface;
+use sdl3::keyboard::Keycode;
+use sdl3::pixels::Color;
+use sdl3::render::Texture;
+use sdl3::surface::Surface;
+use std::time::Duration;
+
+fn main() {
+    let sdl_context = sdl3::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("imvi", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas();
+    let image_surface = Surface::from_file("test_data/DSC02782.jpg").unwrap();
+
+    let texture_creator = canvas.texture_creator();
+    let texture = Texture::from_surface(&image_surface, &texture_creator).unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+        canvas.copy(&texture, None, None).unwrap();
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
